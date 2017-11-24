fb7340181bc898c1500b265b5a8ce34fbf8fb36f
Salvador Guzman <guzmansalv@gmail.com>
2017-11-24 06:41:41 -0800

    code to call google maps

--- src/main/scala/com/github/sguzman/scala/js/scritter/Main.scala
+++ src/main/scala/com/github/sguzman/scala/js/scritter/Main.scala
@@ -1,11 +1,25 @@
 package com.github.sguzman.scala.js.scritter
 
+import google.maps.LatLng
 import org.scalajs.dom
-import dom.document
+import org.scalajs.dom.document
+
+import scala.scalajs.js
 
 object Main {
   def main(args: Array[String]): Unit = {
-    appendPar(document.body, "Hello world")
+    def initialize() = js.Function {
+      val opts = google.maps.MapOptions(
+        center = new LatLng(51.201203, -1.724370),
+        zoom = 8,
+        panControl = false,
+        streetViewControl = false,
+        mapTypeControl = false
+      )
+      val gmap = new google.maps.Map(document.getElementById("map"), opts)
+      ""
+    }
+    google.maps.event.addDomListener(org.scalajs.dom.window, "load", initialize())
   }
 
   def appendPar(targetNode: dom.Node, text: String): Unit = {
