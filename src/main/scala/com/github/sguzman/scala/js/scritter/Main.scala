95c84eb94647f679f99acd000470428fd7716901
Salvador Guzman <guzmansalv@gmail.com>
2017-11-24 07:03:40 -0800

    test out request

--- src/main/scala/com/github/sguzman/scala/js/scritter/Main.scala
+++ src/main/scala/com/github/sguzman/scala/js/scritter/Main.scala
@@ -5,9 +5,27 @@ import org.scalajs.dom.document
 
 import scala.scalajs.js
 
+import fr.hmil.roshttp.HttpRequest
+import monix.execution.Scheduler.Implicits.global
+import scala.util.{Failure, Success}
+import fr.hmil.roshttp.response.SimpleHttpResponse
+
 object Main {
   def main(args: Array[String]): Unit = {
     google.maps.event.addDomListener(org.scalajs.dom.window, "load", initialize())
+
+    val request = HttpRequest("https://tranquil-island-19340.herokuapp.com/")
+    request.send().onComplete({
+      case res:Success[SimpleHttpResponse] => append(document.body, res.get.body)
+      case e: Failure[SimpleHttpResponse] => println("Huston, we got a problem!")
+    })
+  }
+
+  def append[A](node: org.scalajs.dom.Node, text: A): Unit = {
+    val newNode = document.createElement("p")
+    val textNode = document.createTextNode(text.toString)
+    newNode.appendChild(textNode)
+    node.appendChild(newNode)
   }
 
   def initialize() = js.Function {
